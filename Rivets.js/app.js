0896ccb028de1cf88e47d4d0f9409cc3326dd5da
Alan James <alanjames1987@gmail.com>
2014-06-25 05:53:43 -0400

    Commit

--- Rivets.js/app.js
+++ Rivets.js/app.js
@@ -1,14 +1,33 @@
-var model = Marilyn.model('something');
+var model = Marilyn.model('something', function() {
+	this.after('create', function() {
 
-var viewModel = model._collection;
-
-rivets.configure({
-	handler: function(target, event, binding) {
-		console.log('test');
-		this.call(target, event, binding.view.models)
-	}
+	});
 });
 
-rivets.bind($('#things'), {
-	'list': viewModel
+// rivets.configure({
+// 	handler: function(target, event, binding) {
+// 		console.log('test');
+// 		this.call(target, event, binding.view.models)
+// 	}
+// });
+
+var modelTest = [{
+	name: '1',
+}, {
+	name: '2',
+}, {
+	name: '3',
+}];
+
+var view = rivets.bind($('#things'), {
+	'list': modelTest
+});
+
+$('#things').sortable({
+	update: function(event, ui) {
+		view.publish();
+		for (var i = 0, j = modelTest.length; i < j; i++) {
+			console.log(modelTest[i].name);
+		}
+	}
 });
