4bf5ad6c6299c66e979dd0df5334a8d5890a1df2
Alan James <alanjames1987@gmail.com>
2014-11-01 16:29:52 -0400

    Commit

--- Rivets.js/app.js
+++ Rivets.js/app.js
@@ -16,18 +16,21 @@ var modelObject = {
 };
 
 var modelArray = [{
+	'id': 1,
 	'first': 'Alan',
 	'last': 'James',
 	'whole': function() {
 		return this.first + ' ' + this.last;
 	}
 }, {
+	'id': 2,
 	'first': 'Alan2',
 	'last': 'James2',
 	'whole': function() {
 		return this.first + ' ' + this.last;
 	}
 }, {
+	'id': 3,
 	'first': 'Alan3',
 	'last': 'James3',
 	'whole': function() {
@@ -40,11 +43,16 @@ var view = rivets.bind($('body'), {
 	'users': modelArray
 });
 
+var id = modelArray.length;
+
 $('#click').click(function() {
 
+	id++;
+
 	modelArray.push({
-		'first': 'Alan4',
-		'last': 'James4',
+		'id': id,
+		'first': 'Alan' + id,
+		'last': 'James' + id,
 		'whole': function() {
 			return this.first + ' ' + this.last;
 		}
@@ -54,6 +62,22 @@ $('#click').click(function() {
 
 $('#sort').sortable({
 	update: function() {
-		view.publish();
+
+		var sortedIds = $('#sort').sortable('toArray');
+
+		var newArr = sortedIds.map(function(id) {
+			return _.find(modelArray, function(item) {
+				return item.id == id;
+			});
+		});
+
+		while (modelArray.length > 0) {
+			modelArray.pop();
+		}
+
+		for (var i = 0, j = newArr.length; i < j; i++) {
+			modelArray.push(newArr[i]);
+		}
+
 	}
 });
