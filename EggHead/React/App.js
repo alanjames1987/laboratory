0c5832d9cb8ce2233cae8d79267144ef38c9f054
Alan James <alanjames1987@gmail.com>
2016-04-23 14:35:07 -0400

    Commit

---
+++ EggHead/React/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+            </div>
+        );
+    }
+}
+
+// const App = () => <div>Test</div>;
+
+export default App;
