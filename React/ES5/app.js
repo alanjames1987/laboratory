90e738a7b2094f6683299d2f9364addd4ac0dc2b
Alan James <alanjames1987@gmail.com>
2016-09-08 20:53:17 -0400

    Commit

--- React/ES5/app.js
+++
@@ -1,9 +0,0 @@
-class Thing extends React.Component {
-
-	render() {
-		return <div>{this.props.something}</div>
-	}
-
-}
-
-ReactDOM.render(<Thing something="test" />, document.getElementById('app'))
