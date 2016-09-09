9ddd826b3b16ec92cc4a67f21c0d4ca94b7ff2c5
Alan James <alanjames1987@gmail.com>
2016-09-09 01:26:01 -0400

    Commit

--- React/ES6 Babel Webpack/main.js
+++ React/ES6 Babel Webpack/main.js
@@ -6,21 +6,47 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+
 import ControllerHome from './controller/Home';
 import ControllerOther from './controller/Other';
 import Controller404 from './controller/404';
 
 injectTapEventPlugin();
 
-var app = new Sammy('body');
-var content = document.getElementById('app');
+var router = new Sammy('body');
 
-app.get('/#/', function(){
+var app = document.getElementById('app');
+var content = document.getElementById('content');
+
+ReactDOM.render(<MuiThemeProvider>
+	<AppBar
+	    title="Title"
+	    iconElementRight={
+			<IconMenu
+				iconButtonElement={
+					<IconButton><MoreVertIcon /></IconButton>
+				}
+				targetOrigin={{horizontal: 'right', vertical: 'top'}}
+				anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+				<MenuItem primaryText="Home" />
+				<MenuItem primaryText="Other" />
+			</IconMenu>
+		}
+	/>
+</MuiThemeProvider>, app);
+
+router.get('/#/', function(){
 	ReactDOM.render(<MuiThemeProvider><ControllerHome /></MuiThemeProvider>, content);
 });
 
-app.get('/#other', function(){
+router.get('/#other', function(){
 	ReactDOM.render(<MuiThemeProvider><ControllerOther /></MuiThemeProvider>, content);
 });
 
-app.run('#/');
+router.run('#/');
