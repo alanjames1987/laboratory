6d886224c89ca29ff645feeb51b77fd64bd7ce03
Alan James <alanjames1987@gmail.com>
2016-09-08 21:21:44 -0400

    Commit

--- React/ES6 Babel Webpack/main.js
+++ React/ES6 Babel Webpack/main.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import Sammy from 'sammy';
 import React from 'react';
 import {
 	ReactDOM,
@@ -14,32 +16,14 @@ import ControllerHome from './controller/Home';
 import ControllerOther from './controller/Other';
 import Controller404 from './controller/404';
 
-const App = React.createClass({
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/inbox">Inbox</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
-})
+var app = new Sammy('body');
 
-const About = React.createClass({
-  render() {
-    return <h3>About</h3>
-  }
-})
+app.get('/#/', function(){
+	render(<ControllerHome /> , document.getElementById('app'));
+});
 
-render((
-	<Router history={browserHistory}>
-		<Route path="/" component={App}>
-			<Route path="other" component={About} />
-			<Route path="*" component={Controller404} />
-		</Route>
-	</Router>
-), document.getElementById('app'))
+app.get('/#other', function(){
+	render(<ControllerOther /> , document.getElementById('app'));
+});
+
+app.run('#/');
