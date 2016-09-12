2104a526947f960080f58b9eb732e46173e1b741
Alan James <alanjames1987@gmail.com>
2016-09-12 07:15:15 -0400

    Commit

---
+++ Framework7/app.js
@@ -0,0 +1,22 @@
+// Initialize app
+var app = new Framework7();
+
+var $$ = Dom7;
+
+// Add view
+var mainView = app.addView('.view-main', {
+	// Because we want to use dynamic navbar, we need to enable it for this view:
+	dynamicNavbar: true
+});
+
+app.onPageInit('test', function(page) {
+	console.log('test');
+});
+
+$$(document).on('pageInit', function(e) {
+
+	var page = e.detail.page;
+
+	console.log(page);
+
+});
