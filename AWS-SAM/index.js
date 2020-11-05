899edafa12ef2089ff5884cb77c15f9dd63183ae
Alan James <alanjames1987@gmail.com>
2020-11-05 08:50:49 -0500

    Working on tutorial.

--- AWS-SAM/index.js
+++ AWS-SAM/index.js
@@ -2,34 +2,13 @@
 
 console.log('Loading function');
 
-const sendResponse = (statusCode, message, callback) => {
-
-	const response = {
-		statusCode: statusCode,
-		body: JSON.stringify(message),
-	};
-
-	callback(null, response);
-
-};
-
 exports.handler = (event, context, callback) => {
 
-	switch (event.httpMethod) {
-		case 'DELETE':
-			sendResponse(200, 'DELETE', callback);
-			break;
-		case 'GET':
-			sendResponse(200, new Date(), callback);
-			break;
-		case 'POST':
-			sendResponse(200, 'POST', callback);
-			break;
-		case 'PUT':
-			sendResponse(200, 'PUT', callback);
-			break;
-		default:
-			sendResponse(500, 'not supported', callback);
-	}
+	console.log(event)
+
+	callback(null, {
+		statusCode: 200,
+		body: event.httpMethod,
+	});
 
 };
